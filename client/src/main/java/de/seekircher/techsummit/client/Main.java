8ec324639b731b5afcee7840f414a5aff62e4af4
Norbert Seekircher <norbert@seekircher.de>
2014-12-23 20:08:04 +0100

    Added circuit breaker

--- client/src/main/java/de/seekircher/techsummit/client/Main.java
+++ client/src/main/java/de/seekircher/techsummit/client/Main.java
@@ -13,10 +13,19 @@ import java.util.concurrent.TimeUnit;
  * @author snowbird
  */
 public class Main {
+    private static final int NUM_ITERATIONS = 10;
+    private static final int DELAY_TIME_SECS = 5;
+    
     public static void main(String[] args) {
-        Stopwatch stopwatch = new Stopwatch().start();
-        
-        System.out.println(new DemoClient().echo("Norbert", 10, true));
-        System.out.println("Execution took " + stopwatch.stop().elapsed(TimeUnit.MILLISECONDS) + " msecs");
+        DemoClient client = new DemoClient();
+        Stopwatch stopwatch = new Stopwatch();
+    
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
+            stopwatch.reset().start();
+            String response = client.echo("Norbert", DELAY_TIME_SECS, true);
+            
+            System.out.println(String.format(
+                    "Response after %d msecs: %s", stopwatch.stop().elapsed(TimeUnit.MILLISECONDS), response));
+        }
     }
 }
--- server/src/main/java/de/seekircher/techsummit/server/EchoCommand.java
+++ server/src/main/java/de/seekircher/techsummit/server/EchoCommand.java
@@ -17,14 +17,20 @@ public class EchoCommand extends HystrixCommand<String>  {
     private final Integer delaySeconds;
     private final String name;
     
-    private static final int TIMEOUT = 5000;
     private static final HystrixCommandGroupKey MY_HYSTRIX_COMMAND_GROUP_KEY = 
             HystrixCommandGroupKey.Factory.asKey("DemoCommandGroup");
-
+    private static final int TIMEOUT = 3000;
+    private static final int WINDOW_SIZE_MSECS = 60000; //Time window for circuit breaker metrics
+    private static final int REQUEST_VOLUME_THRESHOLD = 3; //Requests to fail in time window until circuit is opened
+    
     public EchoCommand(String name, Integer delaySeconds) {
         super(Setter.withGroupKey(MY_HYSTRIX_COMMAND_GROUP_KEY)
                 .andCommandPropertiesDefaults(
-                        HystrixCommandProperties.Setter().withExecutionIsolationThreadTimeoutInMilliseconds(TIMEOUT)
+                        HystrixCommandProperties.Setter()
+                                .withExecutionIsolationThreadTimeoutInMilliseconds(TIMEOUT)
+                                .withCircuitBreakerEnabled(true)
+                                .withCircuitBreakerRequestVolumeThreshold(REQUEST_VOLUME_THRESHOLD)
+                                .withMetricsRollingStatisticalWindowInMilliseconds(WINDOW_SIZE_MSECS)
                 )
         );
         this.delaySeconds = delaySeconds;
@@ -43,6 +49,10 @@ public class EchoCommand extends HystrixCommand<String>  {
 
     @Override
     protected String getFallback() {
-        return "We are super sorry, but there was an error. Most probably caused by you, the user!";
+        if (!isCircuitBreakerOpen()) {
+            return "We are super sorry, but there was an error. Most probably caused by you, the user!";
+        } else {
+            return "YOU BROKE THE CIRCUIT YOU ****** *****!!!";
+        }
     }
 }
